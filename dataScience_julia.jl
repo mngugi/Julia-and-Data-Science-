bc1721c29198403dc03fb9b71f1f0725d2fcb00d
M.ngugi <mngugi7@gmail.com>
2024-07-29 20:50:34 +0300

    Update dataScience_julia.jl

--- dataScience_julia.jl
+++ dataScience_julia.jl
@@ -2,6 +2,6 @@ using DataFrames
 using CSV
 
 # Example: Reading a CSV file
-df = CSV.read("/home/x1869/26thpcaps.csv", DataFrame)
+df = CSV.read("/*****/x33333/26thpcaps.csv", DataFrame)
 display(df)
 
